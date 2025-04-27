62e116627eb17e68cdabb5a904f78acfd4bb40f9
gVieiraX <gabriel_silvarosa12@hotmail.com>
2025-04-27 15:45:05 -0300

    first commit

---
+++ TypeScript/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+function printObjeto(pessoa) {
+    console.log(pessoa);
+}
+printObjeto({
+    name: "Gabriel",
+    vulgo: "Vieira"
+});
+console.log("Hello World");
---
+++ TypeScript/src/index.ts
@@ -0,0 +1,15 @@
+type heroi ={
+    name:string,
+    vulgo:string
+}
+
+function printObjeto(pessoa:heroi){
+    console.log(pessoa)
+}
+
+printObjeto({
+    name:"Gabriel",
+    vulgo:"Vieira"
+})
+
+console.log("Hello World")
