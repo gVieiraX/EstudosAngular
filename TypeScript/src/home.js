2671e3c4d623277d83a5d5ea08df4e12f7a8fe87
gVieiraX <gabriel_silvarosa12@hotmail.com>
2025-04-29 16:06:08 -0300

    Tipos de dados TypeScript

--- TypeScript/src/home.js
+++ TypeScript/src/home.js
@@ -1,1 +1,35 @@
 "use strict";
+//Tipos primitivos: boolean,number, string
+let ligado = true;
+let idade = 22;
+let nome = "Gabriel";
+let idadesidade = 12.1;
+//Tipos especiais: null, undefined
+let nulo = null;
+let indefinido = undefined;
+//Tipos abrangetes: any,void
+let retorno;
+let qualquer = false; //Pode retornar algum valor imprevisiel,aceita qualaquer coisa
+//Tipo objeto - sem previsibilidade     
+let produto = {
+    name: "Gabriel",
+    idade: 22,
+    estado: "MG"
+};
+let meuProduto = {
+    name: "Tênis",
+    preco: 30,
+    disponivel: true,
+};
+//Arrays
+let dados = ["Gabriel", "Vieira"];
+let dados2 = ["Gabriel", "Vieira"];
+//Array MultiTypes
+let infos = ["Gabriel", true, 2];
+//Tuplas
+let boleto = ["agua", 11, 22];
+//Métodos Arrays
+dados.pop();
+//Datas
+let data = new Date("29-04-2025 16:02");
+console.log(data.toString());
--- TypeScript/src/home.ts
+++ TypeScript/src/home.ts
@@ -1,1 +1,51 @@
-    
+    //Tipos primitivos: boolean,number, string
+let ligado:boolean = true
+let idade:number = 22
+let nome:string = "Gabriel"
+let idadesidade = 12.1
+
+//Tipos especiais: null, undefined
+let nulo:null = null
+let indefinido:undefined = undefined    
+
+//Tipos abrangetes: any,void
+let retorno:void 
+let qualquer:any = false //Pode retornar algum valor imprevisiel,aceita qualaquer coisa
+
+//Tipo objeto - sem previsibilidade     
+let produto:object = {
+    name:"Gabriel",
+    idade:22,
+    estado:"MG"
+};
+
+//Objeto tipado com previsibilidade
+type produtoLoja = {
+    name:string;
+    preco:number;
+    disponivel:true
+};
+
+let meuProduto:produtoLoja = {
+    name: "Tênis",
+    preco: 30,
+    disponivel: true,
+}
+
+//Arrays
+let dados: string[]= ["Gabriel","Vieira"];
+let dados2: Array<string>= ["Gabriel","Vieira"];
+
+//Array MultiTypes
+let infos: (string | number | true)[] =["Gabriel",true, 2]
+
+//Tuplas
+let boleto: [string,number,number] = ["agua",11,22]
+
+//Métodos Arrays
+dados.pop()
+
+//Datas
+
+let data:Date = new Date("29-04-2025 16:02");
+console.log(data.toString())
