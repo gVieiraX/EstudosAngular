ee9d86a013a007e1f1228ae06c5f827e3727d755
gVieiraX <gabriel_silvarosa12@hotmail.com>
2025-05-01 15:41:51 -0300

    Criado os styles para todos os componentes do Blog

--- ProjetoBlog/src/app/app.component.ts
+++ ProjetoBlog/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { BigCardComponent } from "./components/big-card/big-card.component";
 import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
 import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
 import { SamllCardComponent } from "./components/samll-card/samll-card.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, BigCardComponent, MenuBarComponent, MenuTitleComponent, SamllCardComponent],
+  imports: [RouterOutlet, BigCardComponent, MenuBarComponent, MenuTitleComponent, SamllCardComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
