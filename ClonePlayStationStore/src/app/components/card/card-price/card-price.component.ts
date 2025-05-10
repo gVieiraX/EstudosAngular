15f3729305fe22ca99f3c60456f42ffa7d50e65b
gVieiraX <gabriel_silvarosa12@hotmail.com>
2025-05-10 19:34:19 -0300

    Foi criado a estrutura dos componentes,menu e card-component

---
+++ ClonePlayStationStore/src/app/components/card/card-price/card-price.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-price',
+  imports: [],
+  templateUrl: './card-price.component.html',
+  styleUrl: './card-price.component.css'
+})
+export class CardPriceComponent {
+
+}
