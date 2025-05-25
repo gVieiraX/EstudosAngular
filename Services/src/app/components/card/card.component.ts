88b94fa1899e6eefba8f2d49c782c0d05c05f0dd
gVieiraX <gabriel_silvarosa12@hotmail.com>
2025-05-25 15:33:23 -0300

    Utilizando Pipes

--- Services/src/app/components/card/card.component.ts
+++ Services/src/app/components/card/card.component.ts
@@ -1,10 +1,12 @@
 import { Component,OnInit } from '@angular/core';
 import { PokemonServiceService } from '../../services/pokemon-service.service';
 import { PokemonData } from '../../models/pokemonData';
+import { CommonModule } from '@angular/common';
+import { TransformadorPipe } from '../../Pipes/transformador.pipe';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CommonModule,TransformadorPipe],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
